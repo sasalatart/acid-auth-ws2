31a4d244f19b8b0df31db424b50687869b313dc7
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-03 00:54:43 -0300

    add missing delivery_method for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.secrets.mailgun_api_key,
     domain: Rails.application.secrets.mailgun_domain
