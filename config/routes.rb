b31340f6b86d3e2799bf1050334e181d74080bf9
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-03 16:38:25 -0300

    change the verify user URL to /verify_user/:email

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 #
 #    Prefix Verb   URI Pattern               Controller#Action
 #      root GET    /                         users#index
-#           POST   /users/verify/:email      users#verify {:email=>/([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+/}
+#           POST   /verify_user/:email       users#verify {:email=>/([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+/i}
 #     users GET    /users(.:format)          users#index
 #           POST   /users(.:format)          users#create
 #  new_user GET    /users/new(.:format)      users#new
@@ -18,9 +18,9 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
 
-  post 'users/verify/:email', to: 'users#verify',
-                              constraints: { email: VALID_EMAIL_REGEX },
-                              via: :post, format: false
+  post 'verify_user/:email', to: 'users#verify',
+                             constraints: { email: VALID_EMAIL_REGEX },
+                             via: :post, format: false
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
