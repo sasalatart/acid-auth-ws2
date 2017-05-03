9d31a6aab18a75ab0e1776b7b960d30b168f97e7
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-02 21:06:41 -0300

    annotate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# == Route Map
+#
+#    Prefix Verb   URI Pattern               Controller#Action
+#      root GET    /                         users#index
+#           POST   /users/verify/:email      users#verify {:email=>/([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+/}
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
+#
+
 Rails.application.routes.draw do
   VALID_EMAIL_REGEX = /([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+/
 
